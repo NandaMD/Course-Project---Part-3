66b0fb25335844a0e3788a25b46ab21b99d7ce29
NandaMD <fmdemas@gmail.com>
2022-08-03 06:42:29 -0700

    Course project - Part 2 WEEK5 Fernanda Demas

--- Course_Project___Part_1.py
+++ Course_Project___Part_1.py
@@ -1,9 +1,7 @@
+import datetime
+
 print("Payroll Processing\n")
 
-def calc_tax_netpay(total_hours, hourly_rate, tax_rate):
-    income_tax= float(total_hours) * float(hourly_rate) * float(tax_rate)
-    netpay = float(total_hours) * float(hourly_rate) - income_tax
-    return income_tax, netpay
 
 def insert_employee_name():
     employee_name = input("Enter employee name: ")
@@ -25,48 +23,74 @@ def calc_gross_pay(hours, hourly_rate):
     gross_pay = float(hours) * float(hourly_rate)
     return gross_pay
 
-def employee_info(employee_name, hours, hourly_rate, tax_rate, income_tax, gross_pay, netpay):
+def calc_tax_netpay(total_hours, hourly_rate, tax_rate):
+    income_tax= float(total_hours) * float(hourly_rate) * float(tax_rate)
+    netpay = float(total_hours) * float(hourly_rate) - income_tax
+    return income_tax, netpay
+
+def employee_info(from_date, to_date, employee_name, hours, hourly_rate, tax_rate, income_tax, gross_pay, netpay):
     print()
+    print("From date:", from_date.strftime('%m/%d/%y'))
+    print("To date:", to_date.strftime('%m/%d/%y'))
     print("Employee name: ", employee_name)
     print("Hours worked per pay period: ", hours)
+    print("Hourly rate: $", hourly_rate)
+    print("Gross pay: $", gross_pay)
     print("Effective tax rate: ", tax_rate)
     print("Income tax deducted: $", income_tax)
-    print("Gross pay: $", gross_pay)
     print("Net pay for ", employee_name, "is: $", netpay)
     print("*******************************************************\n")
 
-def cumulative_totals(total_employees, total_hours, total_tax, total_gross_pay, total_netpay):
-    print("Total employees: ", total_employees)
-    print("Cumulative hours all employees: ", total_hours)
-    print("Total tax amount: ", total_tax)
-    print("Total gross pay: ", total_gross_pay)
-    print("Total net pay: ", total_netpay)
+def cumulative_totals(total_dict):
+    print("Total employees: ", total_dict['total_employees'])
+    print("Cumulative hours all employees: ", total_dict['total_hours'])
+    print("Total tax amount: $", total_dict['total_tax'])
+    print("Total gross pay: $", total_dict['total_gross_pay'])
+    print("Total net pay: $", total_dict['total_netpay'])
     print()
+    print('*********************************************************\n')
+
+def get_from_to_date():
+    from_date = input("Enter from date in mm/dd/yyyy format: ")
+    from_date = datetime.datetime.strptime(from_date, "%m/%d/%Y").date()
+    to_date = input("Enter to date in mm/dd/yyyy format: ")
+    to_date = datetime.datetime.strptime(to_date, "%m/%d/%Y").date()
+
+    return from_date, to_date
+
+def calc_employee_taxes(employee_list, total_dict):
+    for employee in employee_listt:
+        from_date, to_date, employee_name, hours, hourly_rate, tax_rate = employee
+        gross_pay = calc_gross_pay(hours, hourly_rate)
+        income_tax, netpay = calc_tax_netpay(total_hours, hourly_rate, tax_rate)
+
 
 def main():
-    total_employees = 0
-    total_hours = 0
-    total_gross_pay = 0
-    total_tax = 0
-    total_netpay = 0
+    employee_list = [ ]
+    total_dict={"total_employees": 0,
+    "total_hours":0,
+    "total_gross_pay":0,
+    "total_tax": 0,
+    "total_netpay":0}
 
     while True:
         employee_name = insert_employee_name()
         if employee_name =="end":
             break
+        from_date, to_date = get_from_to_date()
         hours_worked = insert_hours()
         hourly_rate = insert_hourly_rate()
         tax_rate = insert_tax_rate()
         gross_pay = calc_gross_pay(hours_worked, hourly_rate)
         income_tax, netpay= calc_tax_netpay(hours_worked, hourly_rate, tax_rate)
-        employee_info(employee_name, hours_worked, hourly_rate, tax_rate, income_tax, gross_pay, netpay)
+        employee_info(from_date, to_date, employee_name, hours_worked, hourly_rate, tax_rate, income_tax, gross_pay, netpay)
 
-        total_employees += 1
-        total_hours += hours_worked
-        total_tax += income_tax
-        total_gross_pay += gross_pay
-        total_netpay += netpay
-    cumulative_totals(total_employees, total_hours, total_tax, total_gross_pay, total_netpay)
+        total_dict['total_employees'] += 1
+        total_dict['total_hours'] += hours_worked
+        total_dict['total_tax'] += income_tax
+        total_dict['total_gross_pay'] += gross_pay
+        total_dict['total_netpay'] += netpay
+    cumulative_totals(total_dict)
 
 if __name__=="__main__":
     main()
